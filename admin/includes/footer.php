569a3f49f4226653a4d1d9eae3b0e4cf9d894a91
Odhiambo Atieno <odhiamboisatieno@gmail.com>
2021-05-03 19:51:38 +0300

    Update footer.php

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,3 +1,3 @@
 <div class="copyrights">
-	 <p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2019 Sky King Tours. All Rights Reserved </p> <p>Powered By<a href="http://tirotech.co.ke/"> Tiro Tech Solutions</a>
+	 <p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2019 Sky King Tours. All Rights Reserved </p> <p>Developed By<a href="http://seentechs.com/"> Odhiambo Atieno</a>
 </div>	
